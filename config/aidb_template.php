4adfc3df1767b1e0b01046f6f3eccb40f14434ea
Simon Ericsson <simon@krlsg.se>
2021-10-19 20:58:42 +0200

    Added API code

---
+++ config/aidb_template.php
@@ -0,0 +1,10 @@
+<?php
+// vim: sw=2 ts=2 et
+return array(
+	"host" => "some-database.rds.amazonaws.com",
+	"dbname" => "database-name",
+	"user" => "postgres",
+	"password" => "p455w0rd",
+	"auth_password" => "s3cr3t"
+);
+?>
